b6786d354871c8fa864d297272f826126f99ff46
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2024-03-27 17:14:00 -0400

    added apiUrl & apiKey

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 
-
+function searchCity(city){
+    let apiKey = 'o78b4041ct8e052b8e0418f8ffea31bf';
+    let apiUrl =
+      'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+      console.log(apiUrl);
+}
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement =document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
