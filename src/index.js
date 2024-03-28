aaf2e9a87117611102c75f4c25fd44e32fbe46e3
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2024-03-27 21:59:37 -0400

    added humidity  display

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+   let humidityElement = document.querySelector("#humidity");
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  console.log(response.data);
+}
 
-function searchCity(city){
-    let apiKey = 'o78b4041ct8e052b8e0418f8ffea31bf';
-    let apiUrl =
-      'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
-      console.log(apiUrl);
+function searchCity(city) {
+  let apiKey = "o78b4041ct8e052b8e0418f8ffea31bf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput=document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
-    searchCity(searchInput.value);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
-let searchFormElement =document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("paris");
