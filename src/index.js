1e4c0edac25a2c3ce6387d19f70842d0e4d49aae
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2024-04-03 17:35:56 -0400

    improved date

--- src/index.js
+++ src/index.js
@@ -51,6 +51,13 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp){
+  let date =new Date (timestamp * 1000);
+  let days = ["Sun" , "Mon" , "Tue" , "Wed" ,"Thu" , "Fri" , "Sat"];
+return days[date.getDay()];
+}
+
 function getForecast(city){
 apiKey = "o78b4041ct8e052b8e0418f8ffea31bf";
 apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -61,24 +68,30 @@ console.log(apiUrl)
 
 
 function displayForecast(response) {
-  console.log(response);
-  let days = ["tue", "wed", "thu", "fri", "sat"];
+  console.log(response.data);
+ 
   let forcastHtml = "";
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if(index<5){
     forcastHtml =
       forcastHtml +
       `
   <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <img src="${
+    day.condition.icon_url
+  }" class="weather-forecast-icon"/>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-  <strong>15°</strong>
+  <strong>${Math.round(day.temperature.maximum)}°</strong>
   </div>
-  <div class="weather-forecast-temperatur">9°</div>
+  <div class="weather-forecast-temperatur">${Math.round(
+    day.temperature.minimum
+  )}°</div>
   </div>
   </div>
   `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forcastHtml;
